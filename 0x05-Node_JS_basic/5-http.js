fc3dc3118e80cb126f967b99da05d12d397d48ec
Hana00661 <eng.hana95@gmail.com>
2024-09-18 17:23:53 +0400

    database

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,34 @@
+// More complex server with routing
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+// const readFileAsync = require('./3-read_file_async');
+const readSync = require('./2-read_file');
+
+const database = process.argv[2];
+
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    let output = '';
+
+    // Override console.log to capture the output
+    const originalLog = console.log;
+    console.log = function (message) {
+      output += `${message}`;
+    };
+
+    readSync(database);
+
+    // Restore the original console.log
+    console.log = originalLog;
+    res.end(`This is the list of our students\n${output}`);
+  }
+});
+
+app.listen(port, hostname);
+
+module.exports = app;
---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,5 @@
+/**
+ * recreate the small HTTP server using Express
+ * HTTP server should listen on port 1245
+ * It should return plain text
+ * */
